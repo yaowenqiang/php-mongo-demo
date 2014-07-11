67d3c27ada5e3c4e2d69c663ed9d6b8c25e4bdc2
yaowenqiang <yaowenqiang111@163.com>
2014-07-11 19:01:43 +0800

    add the count demo

--- blog/session_handler/distinct.php
+++ blog/session_handler/distinct.php
@@ -3,7 +3,17 @@ require('dbconnection.php');
 $mongo = DBConnection::instantiate();
 //get an instance of MongoDB object
 $db = $mongo->database;
+$collection = $mongo->getCollection('sample_articles');
 $result = $db->command(array('distinct'=>'sample_articles','key'=>'category'));
+//count example via php or mongo shell
+$allRowNums = $collection->count();
+//count with specified condition
+//via mongo shell
+//db.sample_articles.count();
+$someRowNums = $collection->count(array('author'=>'Spock'));
+//via mongo shell
+//db.sample_articles.count({'author':'Spock'});
+
 //mongo command line usage
 //db.sample_articles.distinct('author')
 ?>
